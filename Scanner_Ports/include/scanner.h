aca7c60cded6e1e2b8a693faf22962fa80daa8ed
Bob <EgorOpera@yandex.ru>
2026-03-01 18:39:13 +0300

    revork Scan

--- Scanner_Ports/include/scanner.h
+++ Scanner_Ports/include/scanner.h
@@ -1,9 +1,12 @@
 #ifndef SCAN_H
 #define SCAN_H
-
 #include <string>
 class Scanner{
-    public:
-        bool checkPort(const std::string& ip, int port);
+
+private:
+    const inline static int t{10};
+public:
+    bool checkPort(const std::string& ip, int port);
 };
+
 #endif //SCAN_H
--- Scanner_Ports/src/scanner.cpp
+++ Scanner_Ports/src/scanner.cpp
@@ -1,34 +1,68 @@
 #include "../include/scanner.h"
+#include <sys/socket.h>    // socket, connect, setsockopt
+#include <netinet/in.h>    // sockaddr_in, htons, INADDR_ANY
+#include <arpa/inet.h>     // inet_pton, inet_ntop
+#include <unistd.h>        // close, read, write
+#include <fcntl.h>         // fcntl (для неблокирующего режима)
+#include <sys/select.h>    // select, FD_SET, FD_ZERO
+#include <cerrno>          // errno
+#include <cstring>         // memset, strerror
 #include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-bool Scanner::checkPort(const std::string& ip, int port) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) return false;
-
-    struct sockaddr_in addr;
-    std::memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-
-    if (inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) <= 0) {
-        close(sock);
+bool Scanner::checkPort(const std::string& ip, int port){
+    sockaddr_in sock;
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(port);
+    sock.sin_addr.s_addr = inet_addr(ip.c_str());
+    int client;
+    client = socket(AF_INET, SOCK_STREAM,0);
+    if(client < 0){
+        std::cerr<<"Сокет не создался(возможно не правильный ip-adress), попробуйте ещё раз\n";
         return false;
     }
+    int flags = fcntl(client, F_GETFL, 0);
+    if(flags == -1){
+        close(client);
+        return false;
+    }
+    fcntl(client, F_SETFL, flags | O_NONBLOCK);
 
-    struct timeval timeout;
-    timeout.tv_sec = 2;
-    timeout.tv_usec = 0;
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
-
-    int res = connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-
-    close(sock);
-    return res == 0;
+    int r = connect(client, (struct sockaddr*) &sock, sizeof(sock));
+    if(r == 0){
+        std::cout<<"Порт открыт!\n";
+    }else if(errno == EINPROGRESS){
+        fd_set write;
+        FD_ZERO(&write);
+        FD_SET(client, &write);
+        struct timeval tv;
+        tv.tv_sec = t;
+        tv.tv_usec = 0;
+        int ready = select(client + 1, NULL, &write, NULL, &tv);
+        if(ready == 1){
+                int error;
+                socklen_t len = sizeof(error);
+                if (getsockopt(client, SOL_SOCKET, SO_ERROR, &error, &len) == 0 && error == 0) {
+                    std::cout << "Порт открыт!\n";
+                    close(client);
+                    return true;
+                } else {
+                    std::cerr << "Порт закрыт или фильтруется\n";
+                    close(client);
+                    return false;
+                }
+        }else if(ready == 0){
+            std::cerr<<"Время ожидание превышено\n";
+            close(client);
+            return false;
+        }else{
+            std::cerr<<"Ошибка соединения\n";
+            close(client);
+            return false;
+        }
+    }else{
+        std::cerr<<"Не удалось подключиться\n";
+        return false;
+    }
+    close(client);
+    return false;
 }
