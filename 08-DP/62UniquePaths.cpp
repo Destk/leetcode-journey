a82e87428568f9c4745d47c48dd0833a1c6f354e
Bob <EgorOpera@yandex.ru>
2026-04-12 15:49:49 +0300

    New solved problems ~ #update_62 and UPDATE README.md

--- 08-DP/62UniquePaths.cpp
+++ 08-DP/62UniquePaths.cpp
@@ -2,70 +2,67 @@
 #include<vector>
 #include<limits>
 
-void inp(int& m, int& n){
-    bool c = false;
-    while(!c){
-        std::cout<<"Введите вертикальную m координату: ";
-        std::cin>>m;
-        if(std::cin.fail()){
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            std::cout<<"\nОшибка ввода, попробуйте еще раз\n";
-            continue;
-        }
-        std::cout<<"\nВведите горизонтальную n координату: ";
-        std::cin>>n;
-        if(std::cin.fail()){
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            std::cout<<"\nОшибка ввода, попробуйте еще раз\n";
-            continue;
-        }
-        else{
-            std::cout<<"\nX = "<<m<<" -- "<<"Y = "<<n<<'\n';
-            c = true;
+class Solution {
+private:
+    int m{};
+    int n{};
+public:
+    void error(){
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout<<"\nОшибка ввода, попробуйте ещё раз!\n";
+    }
+    void inp(){
+        std::cout<<"Подсчет уникальных маршрутов для работа, который идет от левого верхнего угла к правому нижнему\nРобот может двигаться только вниз и вправо\n";
+        bool stop{false};
+        while(!stop){
+            std::cout<<"Введите кол-во строк: ";
+            std::cin>>m;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }
+            std::cout<<"\nВведите кол-во столбцов: ";
+            std::cin>>n;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }
+            std::cout<<'\n';
+            stop = true;
         }
     }
-}
-
-int uniquePaths(int m, int n) {
-    if(m == 0 || n == 0) return 0;
-    if(m == 1 && n == 1) return 1;
-    std::vector<std::vector<int>> dp(m, std::vector<int>(n,0));
-    dp[0][0] = 1;
-    for(int i =0; i < m;i++){
-        for(int j = 0; j < n; j++){
-            if(i == 0 && j == 0) continue;
-            if(i == 0){
-                dp[i][j] = dp[i][j-1];
-            }else if(j == 0){
-                dp[i][j] = dp[i-1][j];
-            }else{
-                dp[i][j] = dp[i][j-1]+dp[i-1][j];
+    int uniquePaths() {
+        if(m == 0 || n == 0) return 0;
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++) dp[i][0] = 1;
+        for(int j = 0; j < n; j++) dp[0][j] = 1;
+        for(int i = 1; i < dp.size(); i++){
+            for(int j = 1; j < dp[i].size(); j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];   
             }
         }
+        return dp[m-1][n-1];
     }
-    return dp[m-1][n-1];
-}
-
-void out(int r){
-    if(r != 0){
-        std::cout<<"Кол-во путей в правый нижний угол составило: "<< r <<'\n';
-    }else{
-        std::cout<<"Кол-во путей 0\n";
+    void out(const int r) const{
+        std::cout<<"Кол-во путей составило: "<<r<<'\n';
     }
+};
+void play(){
+    char stop{};
+    do{
+        Solution robot;
+        robot.inp();
+        int r = robot.uniquePaths();
+        robot.out(r);
+        std::cout<<"Продолжить? (y/n): ";
+        std::cin>>stop;
+        std::cin.ignore();
+        std::cout<<'\n';
+    }while(stop == 'y' || stop == 'Y');
 }
 int main(){
-    char s{};
-    do{
-        int m{}, n{};
-        inp(m,n);
-        int r = uniquePaths(m,n);
-        out(r);
-        std::cout<<"Продолжить? (y/n): ";
-        std::cin>>s;
-        std::cout<<'\n';
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-    }while(s == 'y' || s == 'Y');
+    play();
     return 0;
 }
