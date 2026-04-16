1c4fef62ea5b036ab4eb9b954e7ab2d8546dede2
Bob <EgorOpera@yandex.ru>
2026-04-16 14:49:41 +0300

    New solved problems ~ #516LPS_update and UPDATE README.md

--- 08-DP/516LPS.cpp
+++ 08-DP/516LPS.cpp
@@ -14,25 +14,29 @@ public:
     }
     int longestPalindromeSubseq() {
         if(s.empty()) return 0;
-        std::vector<std::vector<int>> dp(s.size(), std::vector<int>(s.size(), 0));
-        for(size_t i = 0; i < dp.size(); i++){
+        int n = s.length();
+        std::vector<std::vector<int>> dp(n, std::vector<int>(n,0));
+        for(int i = 0; i < n; i++){
             dp[i][i] = 1;
         }
-        for(int len = 2; len <= s.size(); len++){
-            for(int i = 0; i <= s.size() - len; i++){
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i+1]) {
+                dp[i][i+1] = 2;
+            } else {
+                dp[i][i+1] = 1;
+            }
+        }
+        for(int len = 3; len <= n; len++){
+            for(int i = 0; i + len - 1 < n; i++){
                 int j = i + len - 1;
                 if(s[i] == s[j]){
-                    if(len == 2){
-                        dp[i][j] = 2;
-                    }else{
-                        dp[i][j] = 2 + dp[i+1][j-1];
-                    }
+                    dp[i][j] = 2 + dp[i+1][j-1];
                 }else{
-                    dp[i][j] = std::max(dp[i+1][j],dp[i][j-1]);
+                    dp[i][j] = std::max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
-        return dp[0][s.size()-1];
+        return dp[0][n-1];
     }
     void out(int r){
         std::cout<<"Маскимальная длина палиндрома: "<<r<<'\n';
