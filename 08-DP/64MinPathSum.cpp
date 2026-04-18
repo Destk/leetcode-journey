a63c31084848c023ea09b8fb096b40ec0399eda1
Bob <EgorOpera@yandex.ru>
2026-04-18 17:49:23 +0300

    New solved problems ~ #64MinPathSum and UPDATE README.md

--- 08-DP/64MinPathSum.cpp
+++ 08-DP/64MinPathSum.cpp
@@ -1,20 +1,89 @@
+#include <iostream>
+#include <vector>
 class Solution {
+private:
+    std::vector<std::vector<int>> grid{};
+    void error(){
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout<<"\nОшибка ввода, попробуйте ещё раз!\n";
+    }
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        std::vector<std::vector<int>> dp(grid.size(), std::vector<int>(grid[0].size(),0));
-        dp[0][0] = grid[0][0];
-        for(size_t i = 1; i < grid[0].size(); i++){
-            dp[0][i] = dp[0][i-1] + grid[0][i]; 
-        }
-        for(size_t i = 1; i < grid.size(); i++){
-            dp[i][0] = dp[i-1][0] + grid[i][0]; 
-        }
-        for(size_t i = 1; i < grid.size(); i++){
-            for(size_t j = 1; j < grid[i].size();j++){
-                dp[i][j] = grid[i][j] + std::min(dp[i-1][j], dp[i][j-1]);
+    void inp(){
+        bool stop{false};
+        while(!stop){
+            std::cout<<"Введите кол-во строк: ";
+            int x{};
+            std::cin>>x;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }
+            std::cout<<"\nВведите кол-во столбцов: ";
+            int y{};
+            std::cin>>y;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }else{
+                grid.resize(x, std::vector<int>(y,0));
+                for(int i = 0; i < x; i++){
+                    for(int j = 0; j < y; j++){
+                        std::cout<<"\nВведите вес пути: ";
+                        int tmp{0};
+                        std::cin>>tmp;
+                        if(std::cin.fail()){
+                            error();
+                            i--;
+                            j--;
+                            continue;
+                        }
+                        grid[i][j] = tmp;
+                    }
+                }
+                std::cout<<'\n';
+                stop = true;
             }
         }
-        return dp[grid.size()-1][grid[0].size()-1];
+    }
+    int minPathSum() {
+        if(grid.empty()) return 0;
+        int s = grid.size();
+        int t = grid[0].size();
+        std::vector<std::vector<int>> dp(s,std::vector<int>(t,0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < s; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j = 1; j < t; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        for(int i = 1; i < s; i++){
+            for(int j = 1; j < grid[i].size();j++){
+                dp[i][j] = std::min(dp[i-1][j], dp[i][j-1]) + grid[i][j]; 
+            }
+        }
+        return dp[s-1][t-1];
+    }
+    void out(const int r) const{
+        std::cout<<"Минимальный путь составил: "<<r<<'\n';
     }
 };
+void play(){
+    char stop{};
+    std::cout<<"Задача на подсчет минимального пути. \n(из левого верхнего угла в правый нижний)\n";
+    do{
+        Solution s;
+        s.inp();
+        int r = s.minPathSum();
+        s.out(r);
+        std::cout<<"Продолжить? (y/n): ";
+        std::cin>>stop;
+        std::cin.ignore();
+        std::cout<<'\n';
+    }while(stop == 'y' || stop == 'Y');
+}
+int main(){
+    play();
+    return 0;
+}
