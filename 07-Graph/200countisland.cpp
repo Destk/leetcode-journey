3adaeba8dd414c52275a79661f81d76b9ebe3694
Bob <EgorOpera@yandex.ru>
2026-04-20 13:42:13 +0300

    New solved problems ~ #200countisland and UPDATE README.md

--- 07-Graph/200countisland.cpp
+++ 07-Graph/200countisland.cpp
@@ -1,60 +1,111 @@
-int numIslands(vector<vector<char>>& grid) {
-    if(grid.empty() || grid[0].empty()) return 0;
-    int countis = 0;
-    std::queue<std::pair<int,int>> q;
-    std::vector<std::vector<bool>> visited(grid.size(), std::vector<bool>(grid[0].size(), false));
-    
-    for(int i = 0; i < grid.size(); i++){
-        for(int j = 0; j < grid[i].size(); j++){
-            if(grid[i][j] == '1' && !visited[i][j]){
-                countis++;
-                q.push({i,j});
-                
-                while(!q.empty()){
-                    std::pair<int, int> curr = q.front();
-                    int ci = curr.first;
-                    int cj = curr.second;
-                    q.pop();
-                    visited[ci][cj] = true;
-                    
-                    // ⬆️ верхний
-                    int nci = ci - 1;
-                    int ncj = cj;
-                    if(nci >= 0 && nci < grid.size() && ncj >= 0 && ncj < grid[0].size() && 
-                       grid[nci][ncj] == '1' && !visited[nci][ncj]){
-                        q.push({nci,ncj});
-                        visited[nci][ncj] = true;
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class Solution {
+private:
+    std::vector<std::vector<char>> grid{};
+public:
+    void error(){
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout<<"\nОшибка ввода, попробуйте ещё раз!\n";
+    }
+    void inp(){
+        bool stop{false};
+        while(!stop){
+            std::cout<<"Введите кол-во строк: ";
+            int x{};
+            std::cin>>x;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }
+            std::cout<<"\nВведите кол-во столбцов: ";
+            int y{};
+            std::cin>>y;
+            if(std::cin.fail()){
+                error();
+                continue;
+            }else{
+                grid.resize(x, std::vector<char>(y,0));
+                for(int i = 0; i < x; i++){
+                    for(int j = 0; j < y; j++){
+                        std::cout<<"\nВведите 1/0: ";
+                        char tmp{};
+                        std::cin>>tmp;
+                        if(std::cin.fail()){
+                            error();
+                            i--;
+                            j--;
+                            continue;
+                        }
+                        grid[i][j] = tmp;
                     }
-                    
-                    // ⬇️ нижний
-                    nci = ci + 1;
-                    ncj = cj;
-                    if(nci >= 0 && nci < grid.size() && ncj >= 0 && ncj < grid[0].size() && 
-                       grid[nci][ncj] == '1' && !visited[nci][ncj]){
-                        q.push({nci,ncj});
-                        visited[nci][ncj] = true;
-                    }
-                    
-                    // ⬅️ левый
-                    nci = ci;
-                    ncj = cj - 1;
-                    if(nci >= 0 && nci < grid.size() && ncj >= 0 && ncj < grid[0].size() && 
-                       grid[nci][ncj] == '1' && !visited[nci][ncj]){
-                        q.push({nci,ncj});
-                        visited[nci][ncj] = true;
-                    }
-                    
-                    // ➡️ правый
-                    nci = ci;
-                    ncj = cj + 1;
-                    if(nci >= 0 && nci < grid.size() && ncj >= 0 && ncj < grid[0].size() && 
-                       grid[nci][ncj] == '1' && !visited[nci][ncj]){
-                        q.push({nci,ncj});
-                        visited[nci][ncj] = true;
-                    }
-                } // ✅ while закрывается здесь
+                }
+                std::cout<<'\n';
+                stop = true;
             }
         }
     }
-    return countis;
+    void BFS(std::queue<std::pair<int,int>>& q, std::vector<std::vector<char>>& grid){
+        while(!q.empty()){
+            std::pair<int,int> cord = q.front();
+            q.pop();
+            int x = cord.first;
+            int y = cord.second;
+            if(x > 0 && grid[x-1][y] == '1'){
+                grid[x-1][y] = '0';
+                q.push({x-1,y});
+            }if( x < grid.size()-1 && grid[x+1][y] == '1'){
+                grid[x+1][y] = '0';
+                q.push({x+1,y});
+            }if( y > 0 && grid[x][y-1] == '1'){
+                grid[x][y-1] = '0';
+                q.push({x,y-1});
+            }if( y < grid[x].size()-1 && grid[x][y+1] == '1'){
+                grid[x][y+1] = '0';
+                q.push({x,y+1});
+            }
+        }
+    }
+    int numIslands() {
+        if(grid.empty()) return 0;
+        int count{};
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == '1'){
+                    std::queue<std::pair<int,int>> q{};
+                    q.push({i,j});
+                    grid[i][j] = '0';
+                    count++;
+                    BFS(q, grid);
+                }
+            }
+        }
+        return count;
+    }
+    void out(const int r) const{
+        std::cout<<"Максимальный кол-во островов: "<<r<<'\n';
+    }
+};
+
+void play(){
+    char stop{};
+    std::cout<<"Задача Подсчет островов\n";
+    do{
+        Solution s;
+        s.inp();
+        int r = s.numIslands();
+        s.out(r);
+        std::cout<<"Продолжить? (y/n): ";
+        std::cin>>stop;
+        std::cin.ignore();
+        std::cout<<'\n';
+    }while(stop == 'y' || stop == 'Y');
+}
+
+int main(){
+    play();
+    return 0;
 }
