97bf40f638b6cacad57404a91352a6d2d1c6c048
Destk <destk@github.com>
2026-05-11 14:13:34 +0300

    New solved problems ~ #303 and UPDATE README.md

---
+++ 08-DP/303.cpp
@@ -0,0 +1,15 @@
+class NumArray {
+private:
+    std::vector<int> pref{};
+public:
+    NumArray(vector<int>& nums) {
+        pref.resize(nums.size()+1, 0);
+        for(int i = 0; i < nums.size(); i++){
+            pref[i+1] = pref[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return pref[right+1] - pref[left];
+    }
+};
