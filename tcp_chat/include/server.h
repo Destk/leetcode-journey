1851afb574f5f9e62863792b838b6306851d9085
Destk <destk@github.com>
2026-05-31 18:12:33 +0300

    3 этап Tcp-chat

--- tcp_chat/include/server.h
+++ tcp_chat/include/server.h
@@ -4,14 +4,23 @@
 #include <netinet/in.h>  // для sockaddr_in
 #include <sys/socket.h>  // для socket
 #include <iostream>
+#include <thread> // для управления потоками
+#include <chrono> // для разграничения по времени
+#include <mutex> //блокировка потоков
+#include <vector>
+#include <algorithm>
 class Server{
     private:
         int server_sock;
         struct sockaddr_in addr;
         int port;
+        std::mutex mtx;
+        std::vector<int> clients;
+        std::mutex clientMutex;
     public:
         Server();
         void bindNaccept();
         ~Server();
+        void handler(int client_sock);
 };
 #endif //SERVER_H 
